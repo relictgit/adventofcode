b0cb87fb4f1b8231ea024187941aa9912ff11f14
Kvoss <kvoss_git@freenet.de>
2022-01-01 20:42:20 +0100

    added member variables to calc_position

--- adventofcode/2021/02/calc_position.cpp
+++ adventofcode/2021/02/calc_position.cpp
@@ -10,28 +10,54 @@ position::~position() {}
 
 position::position(std::string fward,
                 std::string up,
-                std::string down)
+                std::string down,
+                std::string list_input_substr,
+                int result,
+                int pos,
+                int depth,
+                int horizontal_pos,
+                int str_split_pos,
+                int number_substr)
 {
-    set_position(fward, up, down);
+    set_position(fward,
+            up,
+            down,
+            list_input_substr,
+            result,
+            pos,
+            depth,
+            horizontal_pos,
+            str_split_pos,
+            number_substr);
 }
 
 void position::set_position(std::string fward,
         std::string up,
-        std::string down)
+        std::string down,
+        std::string list_input_substr,
+        int result,
+        int pos,
+        int depth,
+        int horizontal_pos,
+        int str_split_pos,
+        int number_substr)
 {
     m_up = up;
     m_down = down;
     m_fward = fward;
+    m_list_input_substr = list_input_substr;
+    m_result = result;
+    m_pos = pos;
+    m_depth = depth;
+    m_horizontal_pos = horizontal_pos;
+    m_str_split_pos = str_split_pos;
+    m_number_substr = number_substr;
 }
 
 int position::get_position()
 {
     reader rd;
     std::vector <std::string>list_input = rd.file_reader();
-    int result = 0;
-    int pos = 0;
-    int depth = 0;
-    int horizontal_pos = 0;
     m_fward = "forward";
     m_down = "down";
     m_up = "up";
@@ -46,38 +72,37 @@ int position::get_position()
 
     for (unsigned long counter = 0; counter < list_input.size(); counter++)
     {
-        int str_split_pos = list_input.at(counter).find(" ");
-        std::string list_input_substr = list_input.at(counter).substr(str_split_pos + 1);
-        int number_substr;
+        m_str_split_pos = list_input.at(counter).find(" ");
+        m_list_input_substr = list_input.at(counter).substr(m_str_split_pos + 1);
         // stoi(list_input_substr) is not functioning
 
-        if (list_input.at(counter).find(m_fward, pos) != std::string::npos)
+        if (list_input.at(counter).find(m_fward, m_pos) != std::string::npos)
         {
-            std::stringstream scontainer(list_input_substr);
-            scontainer >> number_substr;
+            std::stringstream scontainer(m_list_input_substr);
+            scontainer >> m_number_substr;
             /*std::cout << "TESTING: Match was forward: " << list_input.at(counter) << std::endl
                 << "CORESPONDING_NUMBER_AS_STRING: \"" << list_input_substr << "\"" << std::endl
                 << "CORESPONDING_NUMBER_AS_INT: " << number_substr << std::endl;*/
-            horizontal_pos += number_substr;
+            m_horizontal_pos += m_number_substr;
         }
-        else if (list_input.at(counter).find(m_down, pos) != std::string::npos)
+        else if (list_input.at(counter).find(m_down, m_pos) != std::string::npos)
         {
             // std::cout << "TESTING: Match was down: " << list_input.at(counter) << std::endl;
-            std::stringstream scontainer(list_input_substr);
-            scontainer >> number_substr;
-            depth -= number_substr;
+            std::stringstream scontainer(m_list_input_substr);
+            scontainer >> m_number_substr;
+            m_depth -= m_number_substr;
         }
-        else if (list_input.at(counter).find(m_up, pos) != std::string::npos)
+        else if (list_input.at(counter).find(m_up, m_pos) != std::string::npos)
         {
             // std::cout << "TESTING: Match was up: " << list_input.at(counter) << std::endl;
-            std::stringstream scontainer(list_input_substr);
-            scontainer >> number_substr;
-            depth += number_substr;
+            std::stringstream scontainer(m_list_input_substr);
+            scontainer >> m_number_substr;
+            m_depth += m_number_substr;
         }
     }
 
     // result is unexpectedly negative... quickfix will be * (-1)
-    result = horizontal_pos * depth * (-1);
+    m_result = m_horizontal_pos * m_depth * (-1);
 
-    return result;
+    return m_result;
 }
--- adventofcode/2021/02/calc_position.h
+++ adventofcode/2021/02/calc_position.h
@@ -9,20 +9,49 @@ class position
         std::string m_up;
         std::string m_down;
         std::string m_fward;
+        std::string m_list_input_substr;
+        int m_result;
+        int m_pos;
+        int m_depth;
+        int m_horizontal_pos;
+        int m_str_split_pos;
+        int m_number_substr;
+        
 
     public:
         position(std::string fward = "",
                 std::string up = "",
-                std::string down = "");
+                std::string down = "",
+                std::string list_input_substr = "",
+                int result = 0,
+                int pos = 0,
+                int depth = 0,
+                int horizontal_pos = 0,
+                int str_split_pos = 0,
+                int number_substr = 0);
         ~position();
         void set_position(std::string fward = "",
                 std::string up = "",
-                std::string down = "");
+                std::string down = "",
+                std::string list_input_substr = "",
+                int result = 0,
+                int pos = 0,
+                int depth = 0,
+                int horizontal_pos = 0,
+                int str_split_pos = 0,
+                int number_substr = 0);
         int get_position();
 
         std::string get_fward() { return m_fward; }
         std::string get_up() { return m_up; }
         std::string get_down() { return m_down; }
+        std::string get_list_input_substr() { return m_list_input_substr; }
+        int get_result() { return m_result; }
+        int get_pos() { return m_pos; }
+        int get_depth() { return m_depth; }
+        int get_horizontal_pos() { return m_horizontal_pos; }
+        int get_str_split_pos() { return m_str_split_pos; }
+        int get_number_substr() { return m_number_substr; }
 };
 
 #endif // CALC_POSITION_H
