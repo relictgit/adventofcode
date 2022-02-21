dc8a348dcea300064edec4bf28810d4b36badbad
Kvoss <kvoss_git@freenet.de>
2022-02-21 19:32:08 +0100

    preparation: task 2

--- adventofcode/2021/02/main.cpp
+++ adventofcode/2021/02/main.cpp
@@ -2,13 +2,17 @@
 // Date created:    30.12.2021
 // Description:     advent_of_code task 2
 // it is Time to dive :)
-/**Rules
+/**Rules task one
  * forward x    =>  increase horizontal position of value x
  * down x       =>  increase the depth by x units
  * up x         =>  decrease the depth by x units
  */
+/**Rules Task two
+ * forward x    =>  increase horizontal aim by x
+ *              =>  
+ * down x       =>  
+ * up x         =>  */
 #include <iostream>
-
 #include "calc_position.h"
 
 
